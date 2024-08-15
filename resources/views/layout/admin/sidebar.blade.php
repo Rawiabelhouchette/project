eb147fbb84637fd9dc5be3fee27905651f37bd35
billson14 <billalisonhouin@gmail.com>
2024-08-15 17:32:07 +0000

    refactor: Update EntrepriseController and sidebar.blade.php
    - Redirect to the first entreprise's show page for professionals
    - Add logic to check if user is the owner of the entreprise in edit method of EntrepriseController
    - Update sidebar.blade.php to display different options based on user roles

--- resources/views/layout/admin/sidebar.blade.php
+++ resources/views/layout/admin/sidebar.blade.php
@@ -78,7 +78,7 @@
                 </li>
             @endif
 
-            @if (auth()->user()->hasRole('Administrateur') || auth()->user()->hasRole('Professionnel'))
+            @if (auth()->user()->hasRole('Administrateur'))
                 <li class="@yield('entreprise')">
                     <a href="javascript:void(0)"><i class="fa fa-city" aria-hidden="true"></i>Entreprise <span class="fa arrow"></span></a>
                     <ul class="nav nav-second-level">
@@ -111,6 +111,28 @@
                             </a>
                         </li>
                     </ul>
+                </li>
+
+                <li class="@yield('abonnement')">
+                    <a href="javascript:void(0)"><i class="fa fa-credit-card" aria-hidden="true"></i>Abonnement<span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level">
+                        <li>
+                            <a href="{{ route('abonnements.create') }}"><i class="fa fa-circle-o-notch" style="margin-right: 15px;font-size: 16px;"></i>
+                                Ajouter
+                            </a>
+                        </li>
+                        <li>
+                            <a href="{{ route('abonnements.index') }}"><i class="fa fa-circle-o-notch" style="margin-right: 15px;font-size: 16px;"></i>
+                                Rechercher
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            @endif
+
+            @if (auth()->user()->hasRole('Professionnel'))
+                <li class="@yield('entreprise')">
+                    <a href="{{ route('entreprises.show', auth()->user()->entreprises->first()->id) }}"><i class="fa fa-building" aria-hidden="true"></i>Entreprise</a>
                 </li>
 
                 <li class="@yield('abonnement')">
