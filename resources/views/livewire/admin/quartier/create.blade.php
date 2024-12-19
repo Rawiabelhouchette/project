8917a751c852e9b4a8f72ee58fd52365b6e27c5b
Billali SONHOUIN <billalisonhouin@gmail.com>
2024-12-19 16:06:13 +0000

    Refactor quartier creation view for improved layout and user experience

--- resources/views/livewire/admin/quartier/create.blade.php
+++ resources/views/livewire/admin/quartier/create.blade.php
@@ -1,76 +1,53 @@
-<div class="card">
-    <div class="card-header">
-        <h4>{{ $libelle }}</h4>
-    </div>
-
-    <div class="card-body">
+<div class="row">
+    <div class="add-listing-box general-info mrg-bot-25 padd-bot-30 padd-top-25 mrg-l-15 mrg-r-15">
+        <div class="listing-box-header">
+            <i class="fa-files theme-cl"></i>
+            <h3>{{ $libelle }}</h3>
+            <p>Gérez les informations de vos quartiers</p>
+        </div>
         <form wire:submit="store()">
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xl-3">
-                    <div class="row form-group">
-                        <label class="col-md-3 col-sm-3 col-xl-2 required">Pays </label>
-                        <div class="col-md-9 col-sm-9 col-xl-10">
-                            <select class="form-control" required wire:model.live='pays_id'>
-                                <option value="">-- {{ __('Choisir un pays') }} --</option>
-                                @foreach ($pays as $item)
-                                    <option value="{{ $item->id }}">{{ $item->nom }}</option>
-                                @endforeach
-                                @error('pays_id')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </select>
-                        </div>
-                    </div>
+            <div class="row mrg-r-10 mrg-l-10">
+                <div class="col-sm-6">
+                    <label class="required">Pays</label>
+                    <select class="form-control" required wire:model.live='pays_id'>
+                        <option value="">-- {{ __('Choisir un pays') }} --</option>
+                        @foreach ($pays as $item)
+                            <option value="{{ $item->id }}">{{ $item->nom }}</option>
+                        @endforeach
+                        @error('pays_id')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </select>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xl-3">
-                    <div class="row form-group">
-                        <label class="col-md-3 col-sm-3 col-xl-2 required">Ville </label>
-                        <div class="col-md-9 col-sm-9 col-xl-10">
-                            <select class="form-control" required wire:model.defer='ville_id'>
-                                <option value="">-- {{ __('Choisir une ville') }} --</option>
-                                @foreach ($villes as $item)
-                                    <option value="{{ $item->id }}">{{ $item->nom }}</option>
-                                @endforeach
-                                @error('ville_id')
-                                    <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </select>
-                        </div>
-                    </div>
+                <div class="col-sm-6">
+                    <label class="required">Ville</label>
+                    <select class="form-control" required wire:model.defer='ville_id'>
+                        <option value="">-- {{ __('Choisir une ville') }} --</option>
+                        @foreach ($villes as $item)
+                            <option value="{{ $item->id }}">{{ $item->nom }}</option>
+                        @endforeach
+                        @error('ville_id')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </select>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xl-3">
-                    <div class="row form-group">
-                        <label class="col-md-3 col-sm-3 col-xl-2 required">Quartier </label>
-                        <div class="col-md-9 col-sm-9 col-xl-10">
-                            <input class="form-control" type="text" placeholder="{{ __('Nom du quartier') }}" required wire:model.defer='nom'>
-                            @error('nom')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
-                        </div>
-                    </div>
+                <div class="col-sm-6">
+                    <label class="required">Quartier</label>
+                    <input class="form-control" type="text" placeholder="{{ __('Nom du quartier') }}" required wire:model.defer='nom'>
                 </div>
-            </div>
-
-            <div class="row">
-                <div class="col-md-12 col-sm-12 col-xl-3">
-                    <button class="btn theme-btn" type="submit" style="margin-right: 15px;" wire:target='store' wire:loading.attr='disabled'>
-                        <i class="fa fa-save fa-lg" style="margin-right: 10px;"></i>
-                        {{ $buttonLibelle }}
-                    </button>
+                <div class="col-sm-12 padd-top-25">
                     @if ($isEdit)
-                        <button class="btn btn-danger" type="button" style="margin-right: 15px;" wire:loading.attr='disabled' wire:click='exitEdit'>
-                            <i class="fa fa-cancel fa-lg" style="margin-right: 10px;"></i>
-                            Annuler
+                        <button class="btn" type="button" style="margin-right: 15px;" wire:click='exitEdit'>
+                            <i class="fa fa-cancel fa-lg" style="margin-right: 10px;"></i> Annuler
                         </button>
                     @endif
+                    <button class="btn theme-btn btnAdd" type="submit" style="margin-right: 15px;" wire:target='store' wire:loading.attr='disabled'>
+                        <i class="fa fa-{{ $formIcon }} fa-lg" style="margin-right: 10px;"></i>
+                        {{ $buttonLibelle }}
+                    </button>
                 </div>
             </div>
         </form>
+
     </div>
 </div>
