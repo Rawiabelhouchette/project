acdb6f761c5386f8ad5329b4a08d88af77265851
Billali SONHOUIN <billalisonhouin@gmail.com>
2025-03-16 11:54:58 +0000

    feat: Enhance annonce filtering with type and publication status options

--- app/Livewire/Admin/Annonce.php
+++ app/Livewire/Admin/Annonce.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Admin;
 
+use App\Utils\AnnoncesUtils;
 use Livewire\Component;
 use App\Models\User;
 use Livewire\WithPagination;
@@ -15,21 +16,40 @@ class Annonce extends Component
 
     private $perPage = 9;
     public $search = '';
-
-    public function updatingSearch()
-    {
-        $this->resetPage();
-    }
+    public $is_published;
+    public $types;
+    public $type;
 
     public function render()
     {
-        $search = $this->search;
+        $this->types = AnnoncesUtils::getFilterAnnonceTypeList();
+        $search = strtolower($this->search);
         $user = User::find(auth()->user()->id);
-        $annonces = $user->annonces()->where(function ($query) use ($search) {
-            $query->orWhereRaw('LOWER(titre) LIKE ?', ['%' . strtolower($search) . '%'])
-                ->orWhereRaw('LOWER(description) LIKE ?', ['%' . strtolower($search) . '%'])
-                ->orWhereRaw('LOWER(type) LIKE ?', ['%' . strtolower($search) . '%']);
-        })->paginate($this->perPage);
+        $annonces = $user->annonces();
+
+        if ($this->type) {
+            $annonces->where('type', $this->type);
+        }
+
+        if ($this->is_published == '1') {
+            $annonces->where('is_active', 1)
+                ->where('date_validite', '>=', now());
+        } elseif ($this->is_published == '0') {
+            $annonces->where(function ($query) {
+                $query->where('is_active', 0)
+                    ->orWhere('date_validite', '<', now());
+            });
+        }
+
+        if ($search) {
+            $annonces->where(function ($query) use ($search) {
+                $query->orWhereRaw('LOWER(titre) LIKE ?', ["%$search%"])
+                    ->orWhereRaw('LOWER(description) LIKE ?', ["%$search%"])
+                    ->orWhereRaw('LOWER(type) LIKE ?', ["%$search%"]);
+            });
+        }
+
+        $annonces = $annonces->paginate($this->perPage);
 
         return view('livewire.admin.annonce', compact('annonces'));
     }
--- resources/views/livewire/admin/annonce.blade.php
+++ resources/views/livewire/admin/annonce.blade.php
@@ -58,22 +58,37 @@
             width: 100%;
             height: 100%;
         }
+
+        .custom-field-filter {
+            margin-top: 6px !important;
+            margin-bottom: 6px !important;
+            height: 40px !important;
+        }
     </style>
 
     <div class="card">
         <div class="card-body padd-l-0 padd-r-0">
             <div class="col-md-12">
-                <div class="row">
-                    <div class="col-md-6" style="margin-top: 10px;">
-                        <span class="mrg-l-10" id="nbre-favoris">
+                <div class="row" style="display: flex; align-items: center; justify-content: space-between; width: 100%;">
+                    <div class="col-md-4" style="margin-top: 10px;">
+                        <span id="nbre-favoris" class="mrg-l-10">
                             {{ $annonces->firstItem() }}-{{ $annonces->lastItem() }} sur {{ $annonces->total() }}
                             annonce(s)
                         </span>
                     </div>
-                    <div class="col-md-6 text-center">
-                        <input class="form-control" id="favorite_search" type="search" value=""
-                            style="margin-top: 6px; margin-bottom: 6px; height: 35px;" placeholder="Rechercher"
-                            wire:model.live.debounce.500ms='search'>
+                    <div class="col-md-8 text-center" style="display: flex; justify-content: flex-end; gap: 10px;">
+                        <select class="form-control custom-field-filter" wire:model.live="type" style="padding-top: 0; padding-bottom: 0;">
+                            <option value="">Tous les types</option>
+                            @foreach ($types as $type)
+                                <option value="{{ $type->valeur }}">{{ $type->nom }}</option>
+                            @endforeach
+                        </select>
+                        <select class="form-control custom-field-filter" wire:model.live="is_published" style="padding-top: 0; padding-bottom: 0;">
+                            <option value="">Toutes les annonces</option>
+                            <option value="1">Annonces publiées</option>
+                            <option value="0">Annonces non publiées</option>
+                        </select>
+                        <input id="favorite_search" class="form-control custom-field-filter" type="search" value="" placeholder="Rechercher" wire:model.live.debounce.500ms='search'>
                     </div>
                 </div>
             </div>
@@ -86,8 +101,7 @@
                                     <div class="listing-shot-img">
                                         <a href="{{ route('show', $annonce->slug) }}">
                                             @if ($annonce->image)
-                                                <img class="img-responsive"
-                                                    src="{{ asset('storage/' . $annonce->imagePrincipale->chemin) }}" alt="">
+                                                <img class="img-responsive" src="{{ asset('storage/' . $annonce->imagePrincipale->chemin) }}" alt="">
                                             @else
                                                 <img class="img-responsive" src="http://via.placeholder.com/800x800" alt="">
                                             @endif
@@ -107,15 +121,11 @@
                                             <h4>{{ Str::limit($annonce->titre, 24, '...') }}</h4>
                                             <p class="listing-location">{{ $annonce->description_courte }}</p>
                                         </a>
-                                        <a class="listing-shot-edit"
-                                            href="{{ $annonce->annonceable->public_edit_url ?? '#' }}">
-                                            <span class="like-listing alt style-2"><i class="fa fa-pencil"
-                                                    aria-hidden="true"></i></span>
+                                        <a class="listing-shot-edit" href="{{ $annonce->annonceable->public_edit_url ?? '#' }}">
+                                            <span class="like-listing alt style-2"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                                         </a>
-                                        <a class="listing-shot-edit"
-                                            href="{{ $annonce->annonceable->public_edit_url ?? '#' }}">
-                                            <span class="like-listing alt style-2"><i class="fa fa-pencil"
-                                                    aria-hidden="true"></i></span>
+                                        <a class="listing-shot-edit" href="{{ $annonce->annonceable->public_edit_url ?? '#' }}">
+                                            <span class="like-listing alt style-2"><i class="fa fa-pencil" aria-hidden="true"></i></span>
                                         </a>
                                     </div>
                                     <div class="listing-price-info">
@@ -156,8 +166,7 @@
                                             <div class="col-md-12 col-sm-12 col-xs-12 text-center">
                                                 <div class="col-md-12 col-sm-12 col-xs-12 text-center">
                                                     @for ($i = 1; $i <= 5; $i++)
-                                                        <i class="{{ $i <= $annonce->note ? 'color' : '' }} fa fa-star"
-                                                            aria-hidden="true"></i>
+                                                        <i class="{{ $i <= $annonce->note ? 'color' : '' }} fa fa-star" aria-hidden="true"></i>
                                                     @endfor
                                                     &nbsp;&nbsp;
                                                     {{ $annonce->note }}
@@ -172,7 +181,7 @@
                         @empty($annonces->items())
                             <div class="col-md-12 col-sm-12">
                                 <div class="listing-shot grid-style">
-                                    <div class="listing-shot-caption text-center mrg-top-20 mrg-bot-20">
+                                    <div class="listing-shot-caption mrg-top-20 mrg-bot-20 text-center">
                                         <h4>Aucune annonce trouvée</h4>
                                     </div>
                                 </div>
