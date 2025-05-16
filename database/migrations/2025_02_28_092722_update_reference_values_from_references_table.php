4f4c2156840436dc61d9d4430d360d318c0a9074
Billali SONHOUIN <billali.sonhouin@ceet.tg>
2025-05-16 16:27:11 +0000

    fix: remove a migration

--- database/migrations/2025_02_28_092722_update_reference_values_from_references_table.php
+++ database/migrations/2025_02_28_092722_update_reference_values_from_references_table.php
@@ -10,10 +10,10 @@ return new class extends Migration
     public function up(): void
     {
         // renommer la valeur "Accessoires de cuisines" en "Accessoires de cuisine"
-        DB::statement('update annonce_reference_valeur set titre = "Accessoires de cuisine" where titre = "Accessoires de cuisines"');
-        DB::statement('update annonce_reference_valeur set slug = "accessoires-de-cuisine" where slug = "accessoires-de-cuisines"');
-        DB::statement('update `references` set slug_nom = "accessoires-de-cuisine" where nom = "Accessoires de cuisines"');
-        DB::statement('update `references` set nom = "Accessoires de cuisine" where nom = "Accessoires de cuisines"');
+        // DB::statement('update annonce_reference_valeur set titre = "Accessoires de cuisine" where titre = "Accessoires de cuisines"');
+        // DB::statement('update annonce_reference_valeur set slug = "accessoires-de-cuisine" where slug = "accessoires-de-cuisines"');
+        // DB::statement('update `references` set slug_nom = "accessoires-de-cuisine" where nom = "Accessoires de cuisines"');
+        // DB::statement('update `references` set nom = "Accessoires de cuisine" where nom = "Accessoires de cuisines"');
     }
 
     /**
@@ -22,10 +22,10 @@ return new class extends Migration
     public function down(): void
     {
         // renommer la valeur "Accessoires de cuisine" en "Accessoires de cuisines"
-        DB::statement('update annonce_reference_valeur set titre = "Accessoires de cuisines" where titre = "Accessoires de cuisine"');
-        DB::statement('update annonce_reference_valeur set slug = "accessoires-de-cuisines" where slug = "accessoires-de-cuisine"');
-        DB::statement('update `references` set slug_nom = "accessoires-de-cuisines" where nom = "Accessoires de cuisine"');
-        DB::statement('update `references` set nom = "Accessoires de cuisines" where nom = "Accessoires de cuisine"');
+        // DB::statement('update annonce_reference_valeur set titre = "Accessoires de cuisines" where titre = "Accessoires de cuisine"');
+        // DB::statement('update annonce_reference_valeur set slug = "accessoires-de-cuisines" where slug = "accessoires-de-cuisine"');
+        // DB::statement('update `references` set slug_nom = "accessoires-de-cuisines" where nom = "Accessoires de cuisine"');
+        // DB::statement('update `references` set nom = "Accessoires de cuisines" where nom = "Accessoires de cuisine"');
 
     }
 };
